2fcd592330b7990c06534d6b0c5ea9be72d216c7
Pôl <paul.beauduc@epitech.eu>
2020-05-18 11:18:23 +0200

    chore: indenting files

--- src/my_getnbr.c
+++ src/my_getnbr.c
@@ -7,33 +7,33 @@
 
 static int strnbr_to_nbr(int i, int start_nbr, char const *str)
 {
-	long time_nbr = 1;
-	int calcul = 0;
+    long time_nbr = 1;
+    int calcul = 0;
 
-	while ((str[i] >= '0' && str[i] <= '9') && str[i]) {
-		time_nbr = time_nbr * 10;
-		i = i + 1;
-	}
-	time_nbr = time_nbr / 10;
-	while (start_nbr < i) {
-		calcul = calcul + (str[start_nbr] - 48) * time_nbr;
-		start_nbr = start_nbr + 1;
-		time_nbr = time_nbr / 10;
-	}
-	return (calcul);
+    while ((str[i] >= '0' && str[i] <= '9') && str[i]) {
+        time_nbr = time_nbr * 10;
+        i = i + 1;
+    }
+    time_nbr = time_nbr / 10;
+    while (start_nbr < i) {
+        calcul = calcul + (str[start_nbr] - 48) * time_nbr;
+        start_nbr = start_nbr + 1;
+        time_nbr = time_nbr / 10;
+    }
+    return (calcul);
 }
 
 int my_getnbr(char const *str)
 {
-	int i = 0;
-	int negative = 0;
-	long start_nbr = 0;
-	long result = 0;
+    int i = 0;
+    int negative = 0;
+    long start_nbr = 0;
+    long result = 0;
 
-	while ((str[i] < '0' || str[i] > '9') && str[i] != '\0') {
-		i = i + 1;
-		start_nbr = start_nbr + 1;
-	}
-	result = strnbr_to_nbr(i, start_nbr, str);
-	return (result);
+    while ((str[i] < '0' || str[i] > '9') && str[i] != '\0') {
+        i = i + 1;
+        start_nbr = start_nbr + 1;
+    }
+    result = strnbr_to_nbr(i, start_nbr, str);
+    return (result);
 }
--- src/put_action.c
+++ src/put_action.c
@@ -9,18 +9,18 @@
 
 void player_action(int match_nb, int line_nb)
 {
-	my_putstr("Player removed ");
-	my_put_nbr(match_nb);
-	my_putstr(" match(es) from line ");
-	my_put_nbr(line_nb);
-	my_putstr("\n");
+    my_putstr("Player removed ");
+    my_put_nbr(match_nb);
+    my_putstr(" match(es) from line ");
+    my_put_nbr(line_nb);
+    my_putstr("\n");
 }
 
 void ia_action(int result, int line)
 {
-	my_putstr("AI removed ");
-	my_put_nbr(result);
-	my_putstr(" match(es) from line ");
-	my_put_nbr(line);
-	my_putstr("\n");
+    my_putstr("AI removed ");
+    my_put_nbr(result);
+    my_putstr(" match(es) from line ");
+    my_put_nbr(line);
+    my_putstr("\n");
 }
